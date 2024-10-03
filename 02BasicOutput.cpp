466a208e7073a1459ebd81f8b983d0f9babbba77
Alexxa1556 <kimbalane25@gmail.com>
2024-10-03 08:39:13 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Our Lady of Lourdes College Foundation\n";
+	cout << "College of Computer Studies\n";
+	cout << "BSIT 1-B\n";
+	cout << "Alexxa Kimjoy S. Balane\n";
+	cout << "P-2 Guinacutan,Vinzons,Camarines Norte\n";
+	return 0;
+}
